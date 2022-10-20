d1a43583edbbcbd21f1b30a108b611120455618c
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2022-10-20 06:54:47 +0530

    ___5___Added Configuration file

---
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.demo.customer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerConfiguration {
+
+    @Value("${app.useCustomerFakeRepo:false}")
+    private Boolean useCustomerFakeRepo;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Instantiated the config");
+        };
+    }
+
+    @Bean
+    CustomerRepo customerRepo() {
+        System.out.println("useCustomerFakeRepo : " + useCustomerFakeRepo);
+        return useCustomerFakeRepo? new CustomerFakeRepository(): new CustomerRepository();
+    }
+}
